aca03594970fafef67db31eed98602cb556a9740
jws <jws8585@gmail.com>
2017-04-11 23:52:13 +0900

    FullCalendar 공통사용 부분 생성 및 수정, schedule,staff fetch 조건문 수정
    - `initialState` 를 `nested object` 에서 `object`로 변경
    - Daily View에서 날짜 변경에 따른 `schedule`정보 요청 조건문 reducer에 추가

--- hello/src/actions/index.js
+++ hello/src/actions/index.js
@@ -95,24 +95,15 @@ export const receiveStaffs = (shop, json) => ({
   receivedAt: Date.now()
 })
 
-const parseJSON = response => {
-  return response.json();
-}
-
-const requestHeader = {
-  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjBhYjVkOTIxMDIyMjMyYjRjNWQ5NzZkYmNlZWE1MTYyOGNlYzE4NjQzM2JhZGM4ODIxOTc4ZWUwY2ZhMDA1MmZjMzc2MzhkMTNjYjljNzcwIn0.eyJhdWQiOiIxIiwianRpIjoiMGFiNWQ5MjEwMjIyMzJiNGM1ZDk3NmRiY2VlYTUxNjI4Y2VjMTg2NDMzYmFkYzg4MjE5NzhlZTBjZmEwMDUyZmMzNzYzOGQxM2NiOWM3NzAiLCJpYXQiOjE0ODQwMzY1MjEsIm5iZiI6MTQ4NDAzNjUyMSwiZXhwIjoxNzk5NTY5MzIxLCJzdWIiOiI0NTUiLCJzY29wZXMiOltdfQ.r6riSaEns6xgXkYtydsqSJ4F1Joo4EhXfgQ9Vwd3uHrWPtcXcF9e9Kg0R9FoCmDGCkzgGoGEDHh67UDXrXQpFJVWJu7X3p3VdFMW-5YYXtTr0M0Z1eq89ocnwWhe--ro0DL4UHIGz5Fl8Dsu7XKEiTt0azgXPPAMk5nrKIc6lybuwbNk_nKoBcmGRQ7uBoU9w0x22_AjIK6Av814Bl5aK581bJiCPqtOTl_1SFuWabT5CxWM_NCz-nJfMJHAhxo0YheeT27ro7TwolGBNtBZIZYx3KAVf4nf54Er_NOGveClAi9_sjnPDq8H8RADmMrhRXTMUKlyljiuVux0SbN-CeGuNdlBeKngqbSoK5-sMt6iZFUaV6TFWzFuFVIgZzQatNlZkHZpMgVmRg0tBTUdefmH3S9aMkgCCnWianFRymJKI19mcXbPko6GecS7maZNrfKG1fa2RRN0HL_yY0sV4OlX7n7MS40tJygrTRUWBeLNkA5DKBsu7r9cBW_qnhoDTGFyP9vGInTbpikoiqJFE5LInMR4m_tmO3dEBA8-rN6dhohLzBn265fQKU0ntCSibSs11z3UloTQPsVrm1cmjBJmKRa_OwIxBej2toqPsjmLglkr4Cl3-YbB7wBkGPJT3DR2wAqrMKrQ4YUpyoY-3PCXOSm_8E9tdjY0Xc11Yc4',
-  'Accept': 'application/json'
-};
-
 const fetchSchedules = (shop, state) => dispatch => {
-  dispatch(requestSchedules);
+  dispatch(requestSchedules(shop));
 
   return new ScheduleApi(shop).getSchedules(state)
     .then(json => dispatch(receiveSchedules(shop, json)));
 }
 
 const fetchStaffs = (shop, state) => dispatch => {
-  dispatch(requestStaffs);
+  dispatch(requestStaffs(shop));
 
   return new StaffApi(shop).getStaffs(state)
     .then(json => dispatch(receiveStaffs(shop, json)));
@@ -126,6 +117,9 @@ const shouldFetchSchedules = (state, shopID) => {
 
   if(schedules.isFetching)
     return false;
+  
+  if(schedules.receivedAt < Date.now())
+    return true;
 
   return schedules.didInvalidate;
 }
@@ -135,8 +129,12 @@ const shouldFetchStaffs = (state, shopID) => {
 
   if(!staffs)
     return true;
+
   if(staffs.isFetching)
     return false;
+
+  if(!staffs.receivedAt < Date.now())
+    return true;
 
   return staffs.didInvalidate;
 }
@@ -165,16 +163,35 @@ export const invalidateShop = shop => ({
 
 // FullCalendar RELATED ACTIONS
 
-export const fullCalendarStart = start => ({
+const fullCalendarStart = start => ({
   type: types.FULLCALENDAR_START,
-  date: {
-    start: [start]
-  }
+  start
 });
 
-export const fullCalendarEnd = end => ({
+const fullCalendarEnd = end => ({
   type: types.FULLCALENDAR_END,
-  date: {
-    end: [end]
-  }
+  end
 });
+
+const fullCalendarBetween = dateBetween => ({
+  type: types.FULLCALENDAR_BETWEEN,
+  start: dateBetween.start,
+  end: dateBetween.end,
+})
+
+const fullCalendarViewType = viewType => ({
+  type: types.FULLCALENDAR_VIEWTYPE,
+  viewType,
+});
+
+export const setCalendarViewType = viewType => (dispatch, getState) => {
+  return dispatch(fullCalendarViewType(viewType));
+}
+
+export const setCalendarStart = start => (dispatch, getState) => {
+  return dispatch(fullCalendarStart(start));
+}
+
+export const setCalendarEnd = end => (dispatch, getState) => {
+  return dispatch(fullCalendarEnd(end));
+}
--- hello/src/reducers/calendar.js
+++ hello/src/reducers/calendar.js
@@ -2,27 +2,27 @@ import * as types from '../actions/actionType';
 import moment from 'moment';
 
 const initialState = {
-    date: {
-        start: moment().format('YYYY-MM-DD'),
-        end: moment().format('YYYY-MM-DD'),    
-    }
+    viewType: 'agendaDay',
+    start: moment().format('YYYY-MM-DD'),
+    end: moment().format('YYYY-MM-DD'),
 }
 
-export const calendarDate = (state = initialState, action) => {
+export const calendarConfig = (state = initialState, action) => {
     switch (action.type) {
         case types.FULLCALENDAR_START:
             return {
                 ...state,
-                date: {
-                    start: state.start,
-                },
+                start: state.start,
             }
         case types.FULLCALENDAR_END:
             return {
                 ...state,
-                date: {
-                    end: state.end,
-                },
+                end: state.end,
+            }
+        case types.FULLCALENDAR_VIEWTYPE:
+            return {
+                ...state,                
+                viewType: action.viewType,                
             }
         default:
             return state;
