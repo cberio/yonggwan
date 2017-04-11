eebaf6a961c9902a64883a8ec63b665873e25885
jws <jws8585@gmail.com>
2017-04-11 23:40:29 +0900

    FullCalendar 공통사용 부분 생성 및 수정, schedule,staff fetch 조건문 수정
    - `initialState`를 `nested object`에서 일반 `object`로 변경
    - 날짜 변경에 따른 `schedule`정보 재 요청시 필요한 조건문 추가

--- hello/src/reducers/calendar.js
+++ hello/src/reducers/calendar.js
@@ -1,28 +1,28 @@
 import * as types from '../actions/actionType';
 import moment from 'moment';
 
-const initialState = {
-    date: {
-        start: moment().format('YYYY-MM-DD'),
-        end: moment().format('YYYY-MM-DD'),    
-    }
+const initialState = {    
+    viewType: 'agendaDay',
+    start: moment().format('YYYY-MM-DD'),
+    end: moment().format('YYYY-MM-DD'),        
 }
 
-export const calendarDate = (state = initialState, action) => {
+export const calendarConfig = (state = initialState, action) => {
     switch (action.type) {
         case types.FULLCALENDAR_START:
             return {
                 ...state,
-                date: {
-                    start: state.start,
-                },
+                start: action.start,
             }
         case types.FULLCALENDAR_END:
             return {
                 ...state,
-                date: {
-                    end: state.end,
-                },
+                end: action.end,
+            }
+        case types.FULLCALENDAR_VIEWTYPE:
+            return {
+                ...state,                
+                viewType: action.viewType,                
             }
         default:
             return state;
