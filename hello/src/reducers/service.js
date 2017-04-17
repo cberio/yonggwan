afc45b783033fdd7968d90af1a5290be1fb796c9
jws <jws8585@gmail.com>
2017-04-17 20:43:51 +0900

    service 관련 reducer 생성

---
+++ hello/src/reducers/service.js
@@ -0,0 +1,41 @@
+import * as types from '../actions/actionType';
+
+const initialState = {
+  isFetching: false,
+  didInvalidate: false,
+  services: {},
+};
+
+const services = (state = initialState, action) => {
+    switch(action.type) {
+        case types.REQUEST_SERVICE:
+            return {
+                ...state,
+                isFetching: true,
+                didInvalidate: false,
+            }
+        case types.RECEIVE_SERVICE:
+            return {
+                ...state,
+                isFetching: false,
+                didInvalidate: false,
+                services: action.services,
+                receivedAt: action.receivedAt,
+            }
+        default:
+            return state;
+    }
+}
+
+export const getServicesBySelectedShopID = (state = {}, action) => {
+    switch (action.type) {
+        case types.REQUEST_SERVICE:
+        case types.RECEIVE_SERVICE:
+            return {
+                ...state,
+                [action.shop]: services(state[action.shop], action),
+            }
+        default:
+            return state;
+    }
+}
